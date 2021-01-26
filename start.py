cf49665ebf1ca626910db51d8acc7ef687f20e76
Dominic Buehler <dominic.buehler@gmx.de>
2021-01-26 22:42:01 +0100

    Updated start.py with homepages and exception handling
    - added all missing homepages
    - reduced scheduled runs to 1 minute for testing
    - catching all exceptions and printing them. Based on that, exception handling will be created after testruns.

--- start.py
+++ start.py
@@ -3,22 +3,29 @@ import schedule
 from decouple import config
 from html_update_checker.user import User
 from html_update_checker.homepage import Homepage
-from html_update_checker.episode_parser_implementations.dragonball_super_anime import DragonballSuperAnimeEpisodeParser
+from html_update_checker.episode_parser_implementations.mediaitem import MediaitemEpisodeParser
+from html_update_checker.episode_parser_implementations.sagatable import SagatableEpisodeParser
 
 #################################
 #   Create users to be notified #
 #################################
 user = User(config("USER_EMAIL"))
 
-#########################
-#   Create homepages    #
-#########################
-dragonball_super_anime = Homepage("https://dragonball-tube.com/dragonball-super-episoden-streams", DragonballSuperAnimeEpisodeParser)
-
 #################################
 #   Register user for updates   #
 #################################
-user.add_homepage_notifications(dragonball_super_anime)
+user.add_homepage_notifications([
+    Homepage("https://dragonball-tube.com/dragonball-super-episoden-streams", MediaitemEpisodeParser),
+    Homepage("http://dragonball-tube.com/dragonball-super-mangaliste", MediaitemEpisodeParser),
+    Homepage("http://dragonball-tube.com/galactic-patrol-mangaliste", MediaitemEpisodeParser),
+    Homepage("https://onepiece-tube.com/episoden-streams", MediaitemEpisodeParser),
+    Homepage("https://onepiece-tube.com/kapitel-mangaliste", SagatableEpisodeParser),
+    Homepage("http://fairytail-tube.org/episoden-streams", MediaitemEpisodeParser),
+    Homepage("http://fairytail-tube.org/100-years-quest-mangaliste", MediaitemEpisodeParser),
+    Homepage("http://fairytail-tube.org/edens-zero-mangaliste", MediaitemEpisodeParser),
+    Homepage("http://naruto-tube.org/boruto-episoden-streams", MediaitemEpisodeParser),
+    Homepage("http://naruto-tube.org/boruto-kapitel-mangaliste", MediaitemEpisodeParser),
+])
 
 #################################################
 #   Regularly fetch episodes and notify users   #
@@ -28,8 +35,12 @@ def update_episodes_and_notify_users():
         homepage.episodes_update()
     user.send_updates()
 
-schedule.every(1).hour.do(update_episodes_and_notify_users)
+schedule.every(1).minute.do(update_episodes_and_notify_users)
 
 while True:
-    schedule.run_pending()
+    try:
+        schedule.run_pending()
+    except Exception as e:
+        print(e)
+    print("Successfully ran checks")
     time.sleep(60)
